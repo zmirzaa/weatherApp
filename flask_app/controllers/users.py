779e78252e86c596f86ffb6a287179807a6be8ec
Zainab Mirza <gotzainab1@hotmail.com>
2023-03-08 15:06:13 -0800

    added details

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,10 +1,11 @@
+from datetime import datetime
 from flask_app import app 
 from flask import render_template,render_template, request, redirect, session
 import os
 import requests 
 
 
-BASE_URL = "https://api.openweathermap.org/data/2.5"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
 
 
 
@@ -17,13 +18,17 @@ def dashboard():
 
 @app.route('/search', methods=['POST'])
 def getWeather():
-    zip_code = request.form['zip']
-    r = requests.get(f"{BASE_URL}/weather?zip={zip_code}&units=imperial&appid={os.environ.get('FLASK_APP_API_KEY')}")
-    
-    session["temp"] = "{0:.2f}".format(r.json()['main']['temp']) 
-    session["weather"]= r.json()["weather"][0]["main"]
-    session["city"]= r.json()["name"]
-
+    city_name = request.form['city']
+    state_code = request.form['state']
+    r = requests.get(f"{BASE_URL}q={city_name},{state_code},&units=imperial&appid={os.environ.get('FLASK_APP_API_KEY')}").json()
+    if r['cod'] == '404': 
+        print("Sorry, city not found.")
+    else:
+        session["temp"] = "{0:.2f}".format(r['main']['temp']) 
+        session["weather"]= r["weather"][0]["description"]
+        session["city"]= r["name"]
+        session['icon'] = r['weather'][0]['icon']
+        session['dateTime'] = datetime.now().strftime("%b %d %Y | %I:%M %p")
     return redirect('/weather')
 
 @app.route('/weather')
